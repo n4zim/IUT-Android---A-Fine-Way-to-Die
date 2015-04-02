83df21b97232fe2f56cb7bda03b2f4ac93a79f6b
Nazim Lachter <lachtater@gmail.com>
2015-04-02 12:21:19 +0200

    Jeu Màj

--- app/src/main/java/nazimlachter/afinewaytodie/Game.java
+++ app/src/main/java/nazimlachter/afinewaytodie/Game.java
@@ -8,7 +8,7 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
-import android.view.SurfaceView;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.RelativeLayout;
@@ -25,12 +25,13 @@ public class Game extends Activity {
 
     float bpm = 103;
     long beat = (long) ((float) (60/bpm) * 1000);
-    int currentBeat = 0;
+    int currentBeat, bigBeat, score, diviseWtf = 0;
+    Boolean wtfMode = false;
 
     private MediaPlayer mPlayer;
 
-    TextView beatDisplay;
-    SurfaceView swBack;
+    TextView beatDisplay, scoreDisplay, levelDisplay;
+    RelativeLayout gameLayout;
 
     Timer timer;
     TimerTask timerTask;
@@ -40,13 +41,26 @@ public class Game extends Activity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        if (android.os.Build.VERSION.SDK_INT >= 19)
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
         setContentView(R.layout.activity_game);
-        swBack = (SurfaceView)findViewById(R.id.surfaceView);
+        gameLayout = (RelativeLayout)findViewById(R.id.gamelay);
 
         mPlayer = MediaPlayer.create(this, R.raw.music);
         beatDisplay = (TextView)findViewById(R.id.beatDisplay);
+        scoreDisplay = (TextView)findViewById(R.id.score);
+        levelDisplay = (TextView)findViewById(R.id.level);
         mPlayer.start();
 
         startTimer();
@@ -59,8 +73,7 @@ public class Game extends Activity {
                 }
 
                 Intent intent = new Intent(Game.this, Result.class);
-                intent.putExtra("TIME", ""+currentBeat);
-                intent.putExtra("SCORE", "100");
+                intent.putExtra("SCORE", ""+score);
 
                 startActivity(intent);
                 finish();
@@ -73,9 +86,9 @@ public class Game extends Activity {
         timer = new Timer();
         initTimer();
         try {
-            timer.schedule(timerTask, new Date(), beat);
+            timer.schedule(timerTask, new Date(), (beat/4));
         } catch (IllegalArgumentException e) {
-            Log.e("ERROR", "Illegal argument : " + beat);
+            Log.e("ERROR", "Illegal argument : " + (beat/4));
         }
     }
 
@@ -85,14 +98,95 @@ public class Game extends Activity {
                 handler.post(new Runnable() {
                     public void run() {
                         currentBeat++;
-                        //if(!(((currentBeat-1) % 4) == 0)) {
-                            beatDisplay.setText("--- " + ((currentBeat % 4)+1) + " ---"+"\n"+currentBeat);
-                        //} else {
-                            //beatDisplay.setText("- - " + ((currentBeat % 4)+1) +" - -"+"\n"+currentBeat);
-                        //}
 
+                        Random rnd = new Random();
 
-                        swBack.setBackgroundColor(Color.parseColor("#" + Integer.toHexString((rand.nextInt((16777215 - 1) + 1) + 1))));//0xFF00FF00);
+                        if(!wtfMode) {
+                            diviseWtf = 4;
+                        } else {
+                            diviseWtf = 1;
+                        }
+
+                        if((((currentBeat-1) % diviseWtf) == 0))
+                            gameLayout.setBackgroundColor(Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256)));
+
+                        if((((currentBeat-1) % 4) == 0)) bigBeat++;
+
+                        beatDisplay.setText(
+                                "Progression : " + bigBeat+"/363 (" + ((currentBeat % 4)+1) + "/4)\n"
+                                +"Longueur : " + currentBeat + "/1457"
+                        );
+
+                        scoreDisplay.setText("" + ++score);
+
+                        switch (bigBeat) {
+                            case 8: // Whaooo
+                                levelDisplay.setText("WHAOW");
+                                wtfMode = true;
+                                break;
+
+                            case 12: // DROP #1
+                                levelDisplay.setText("Niveau 1");
+                                wtfMode = false;
+                                break;
+
+                            case 28:
+                                levelDisplay.setText("Niveau 2");
+                                break;
+
+                            case 44:
+                                levelDisplay.setText("Niveau 3");
+                                break;
+
+                            case 60:
+                                levelDisplay.setText("Niveau 4");
+                                break;
+
+                            case 80:
+                                levelDisplay.setText("GET READY");
+                                wtfMode = true;
+                                break;
+
+                            case 84: // DROP #2
+                                levelDisplay.setText("Niveau 5");
+                                wtfMode = false;
+                                break;
+
+                            case 116:
+                                levelDisplay.setText("Niveau 6");
+                                break;
+
+                            case 148: // Chant
+                                levelDisplay.setText("COOL MODE");
+                                break;
+
+                            case 180:
+                                levelDisplay.setText("VERY COOL MODE");
+                                break;
+
+                            case 212:
+                                levelDisplay.setText("STILL COOL");
+                                break;
+
+                            case 244:
+                                levelDisplay.setText("OWH...");
+                                break;
+
+                            case 260:
+                                levelDisplay.setText("...YES !");
+                                break;
+
+                            case 273: // Whaooo
+                                levelDisplay.setText("WHAOW");
+                                wtfMode = true;
+                                break;
+
+                            case 277: // DROP #3
+                                levelDisplay.setText("Niveau 7");
+                                wtfMode = false;
+                                break;
+                        }
+
                     }
                 });
             }
--- app/src/main/java/nazimlachter/afinewaytodie/Result.java
+++ app/src/main/java/nazimlachter/afinewaytodie/Result.java
@@ -6,19 +6,16 @@ import android.widget.TextView;
 
 public class Result extends Activity {
 
-    TextView tvScore, tvTime;
+    TextView tvScore;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
 
         tvScore = (TextView)findViewById(R.id.score);
-        tvTime = (TextView)findViewById(R.id.time);
 
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
-            String time = extras.getString("TIME");
-            if (time != null) { tvTime.setText(time); }
             String score = extras.getString("SCORE");
             if (score != null) { tvScore.setText(score); }
         }
